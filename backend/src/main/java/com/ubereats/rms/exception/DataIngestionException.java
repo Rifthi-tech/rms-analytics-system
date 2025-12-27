3c6eadcafe2a3f4583a056af63d543531e07f310
Rifthi-tech <Rifthia66@gmail.com>
2025-12-27 17:53:58 +0300

    commit: 4th time all the File added

--- backend/src/main/java/com/ubereats/rms/exception/DataIngestionException.java
+++ backend/src/main/java/com/ubereats/rms/exception/DataIngestionException.java
@@ -21,27 +21,4 @@ public class DataIngestionException extends RuntimeException {
     public DataIngestionException(int lineNumber, String error) {
         super(String.format("Error at line %d: %s", lineNumber, error));
     }
-}package com.ubereats.rms.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class DataIngestionException extends RuntimeException {
-
-    public DataIngestionException(String message) {
-        super(message);
-    }
-
-    public DataIngestionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public DataIngestionException(String fileName, String error, Throwable cause) {
-        super(String.format("Failed to ingest data from file '%s': %s", fileName, error), cause);
-    }
-
-    public DataIngestionException(int lineNumber, String error) {
-        super(String.format("Error at line %d: %s", lineNumber, error));
-    }
 }
